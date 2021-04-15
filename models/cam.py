3659eb77bef148319ab08d70f8ba461d790c40f8
Pujan Dave <pujandave25@gmail.com>
2021-04-15 06:44:23 +0000

    Code Cleanup: Move some common functionalities to util.py

---
+++ models/cam.py
@@ -0,0 +1,76 @@
+'''
+Multi-class Multi-label Grad-CAM algorithm.
+
+Inspired from the multi-class single-label algorithm presented in the following paper:
+
+    Selvaraju, R.R., Cogswell, M., Das, A., Vedantam, R., Parikh, D. and Batra, D., 2017.
+    Grad-cam: Visual explanations from deep networks via gradient-based localization.
+    In Proceedings of the IEEE international conference on computer vision (pp. 618-626).
+'''
+
+from fastai.vision.all import *
+import pandas as pd
+import util
+
+
+def _get_last_layer_activations(model, loss_fn, x, classes):
+    with hook_output(model[0]) as hook_acts:
+        with hook_output(model[0], grad=True) as hook_grads:
+            preds = model(x)
+            loss_fn(preds[0], classes).backward()
+    return hook_acts, hook_grads, preds
+
+
+def _show_activations_heatmap(model, learn, idx, ax1, ax2):
+    image, classes = learn.dls.valid_ds[idx]
+    
+    # Transform the image as per the dataloader
+    # split_idx=1 for validation and test set (center crop)
+    image = learn.dls.after_item[0](image, split_idx=1)
+    true_labels = learn.dls.vocab[classes == 1]
+    x, = first(learn.dls.test_dl([image]))
+    classes = classes.cuda()
+    x = x.cuda()
+
+    hook_acts, hook_grads, preds = _get_last_layer_activations(model, learn.loss_func, x, classes)
+    acts = hook_acts.stored[0].cpu()
+    grads = hook_grads.stored[0][0].cpu()
+    grads_chan = grads.mean(1).mean(1)
+    mult = F.relu((acts * grads_chan[...,None,None]).mean(0))
+        
+    pred_labels = util.get_predictions(preds, learn.dls.vocab, thresh=0.15)
+    
+    image.show(ax1, extent=(0,144,144,0))
+    image.show(ax2, extent=(0,144,144,0))
+    
+    ax2.imshow(mult, alpha=0.6, extent=(0,144,144,0),
+              interpolation='bilinear', cmap='magma')
+    
+    title = f'True: {true_labels}\n    Pred: {pred_labels}\n'
+    
+    return title
+
+
+def plot_cam(learn):
+    model = learn.model.eval()
+    model = model.cuda()
+    val_size = len(learn.dls.valid_ds)
+
+    fig, ax = plt.subplots(3, 3*2)
+    fig.set_size_inches(12,6)
+    
+    title = ''
+
+    for i in range(3):
+        for j in range(0, 6, 2):
+            idx = random.randint(0, val_size)
+            count = 3*i + int(j/2)
+            image_id = chr(ord('A') + count)
+            sub_title = _show_activations_heatmap(model, learn, idx, ax[i, j], ax[i, j+1])
+            ax[i, j].set_title(f'{image_id}')
+            ax[i, j+1].set_title(f'{image_id}-CAM')
+            title += f'{image_id}. {sub_title}\n'
+    
+    fig.text(0.15, -0.65, title)
+
+    plt.show()
---
+++ models/util.py
@@ -0,0 +1,113 @@
+from fastai.vision.all import *
+import pandas as pd
+
+
+class ChexpertLearner(Learner):
+    """ Learner wrapper specifically
+        created for CheXpert
+    """
+    
+    def __init__(self, dls, arch, **kwargs):
+        # For guide on all the input parameters,
+        # check doc for cnn_learner
+        self.learn = cnn_learner(dls, arch, **kwargs)
+    
+    def learn_model(self, use_saved=False,                
+                    # other args for Learner.fine_tune
+                    **kwargs):
+        """ Load a previously saved model or train a new model """
+
+        saved_model_name = f'{self.learn.arch.__name__}-chexpert'
+
+        if use_saved:
+            try:
+                self.learn.load(saved_model_name)
+                return
+            except FileNotFoundError as e:
+                print(f'Could not find saved model {saved_model_name}.')
+
+        print('Learning new model by training...')
+        
+        torch.cuda.empty_cache()
+
+        # `fine_tune` first freezes the body and then updates only head weights for freeze_epochs
+        # then it unfreezes the body and updates all weights for epochs.
+        # Internally it uses `fit_one_cycle` for cyclical learning rates:
+        # Refer: https://iconof.com/1cycle-learning-rate-policy/
+        # Citation:
+        #     Smith LN. Cyclical learning rates for training neural networks.
+        #     In 2017 IEEE winter conference on applications of computer vision
+        #     (WACV) 2017 Mar 24 (pp. 464-472). IEEE.
+
+        self.learn.fine_tune(**kwargs)
+        
+        self.learn.save(saved_model_name)
+        print(f'{saved_model_name}: model saved')
+
+
+def get_predictions(preds, vocab, thresh=0.15):
+    """Returns predictions >= thresh"""
+    pred_labels = []
+    pred_tuples = []
+    prob_preds = torch.sigmoid(preds[0])
+    mask = prob_preds >= thresh
+    significant_labels = vocab[mask]
+    significant_preds = prob_preds[mask]
+    
+    for i in range(len(significant_labels)):
+        pred_tuples.append((significant_labels[i], significant_preds[i].item()))
+    
+    pred_tuples.sort(key = lambda x: x[1])
+    
+    for tup in reversed(pred_tuples):
+        pred_labels.append(f'{tup[0]}: {tup[1]:.3f}')
+    
+    return pred_labels
+
+
+def chexpert_data_loader(reparse=False, img_size=224, bs=64):
+    """ Load the CheXpert dataset.
+        Try loading from the saved chexpert-small.pkl
+        if it exists and reparse is not requested.
+        If the data is reparsed then save it to chexpert-small.pkl
+        
+        Following params only used if reparsing:
+            
+            img_size: Resize the images to size x size
+            bs: batch size
+        
+        NOTE: This assumes that the raw dataset is located at
+                /storage/archive/CheXpert-v1.0-small or as a
+                zip file /storage/archive/CheXpert-v1.0-small.zip
+    """
+    
+    chexpert = Path('/storage/archive/CheXpert-v1.0-small')
+    saved_pkl = Path('../dataset/chexpert-small.pkl')
+    
+    if saved_pkl.exists() and not reparse:
+        dls = torch.load(saved_pkl)
+        labels = list(dls.items.iloc[:,6:].columns.values)
+    else:
+        if not chexpert.exists():
+            file_extract(chexpert.parent/(chexpert.name +'.zip'))
+            
+        train_df = pd.read_csv(chexpert/'train.csv')
+        valid_df = pd.read_csv(chexpert/'valid.csv')
+        train_df.insert(loc=1, column='Valid', value=False)
+        valid_df.insert(loc=1, column='Valid', value=True)
+        cat_df = pd.concat([train_df, valid_df]).fillna(0).replace(-1, 0)
+
+        labels = list(cat_df.iloc[:,6:].columns.values)
+
+        # we resize so that the larger dimension is match and crop 
+        # (randomly on the training set, center crop for the validation set)
+        # Using batchsize of 128 for trainng and validation
+        dls = ImageDataLoaders.from_df(
+        df=cat_df, path=chexpert, folder='/storage/archive/',
+        label_col=labels, y_block=MultiCategoryBlock(encoded=True, vocab=labels),
+        item_tfms=Resize(img_size), bs=bs, val_bs=bs)
+
+        torch.save(dls, saved_pkl)
+    
+    return dls, labels
+
